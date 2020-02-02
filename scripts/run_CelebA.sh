4c8cf1f373aeef6616704c9d1c89d7e556cc9ff0
kam1107 <kam1107@users.noreply.github.com>
2020-02-02 11:28:57 +0800

    Update run scripts

--- scripts/run_CelebA.sh
+++ scripts/run_CelebA.sh
@@ -9,7 +9,7 @@ LOG_NAME="./LOG/log_"$NAME".txt";
 if [ ! -f  $LOG_NAME ]; then
 	OMP_NUM_THREADS=1 python3 train.py --lr_D .0002 --lr_G .0002 --beta1 .50 --beta2 .999 \
 	--gen_every 10000 --print_every 1000 --G_h_size 32 --D_h_size 32 --gen_extra_images 5000 \
-	--input_folder '/home/danny/work/ICCV_2019/Datasets/CelebA_HD_Full/gen_by_DS_256' \
+	--input_folder 'path/to/dataset' \
 	--image_size 256 \
 	--total_iters 520000 \
 	--seed $SEED \
